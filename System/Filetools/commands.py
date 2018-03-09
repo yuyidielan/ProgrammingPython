9f436a653674b12574a4ddaab87b832835424db8
yuyidielan <2267476472@qq.com>
2018-03-10 00:49:38 +0800

    Add files via upload

---
+++ System/Filetools/commands.py
@@ -0,0 +1,13 @@
+from sys import argv
+from scanfile import scanner
+class UnknownCommand(Exception):pass
+def processLine(line):
+    if line[0]=='*':
+        print("MS.)",line[1:-1])
+    elif line[0]=='+':
+        print("Mr.",line[1:-1])
+    else:
+        raise UnknownCommand(line)
+filename='data.txt'
+if len(argv)==2:filename=argv[1]
+scanner(filename,processLine)
